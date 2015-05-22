a572f6cf5dfddcf5b864c0df221eb8ba62c7e4ee
Timothy Gamboa <tgamboa@swbell.net>
2015-05-21 22:34:13 -0500

    Player/enemy movement & boundary limits complete

--- js/app.js
+++ js/app.js
@@ -1,24 +1,19 @@
 // Enemies our player must avoid
 var Enemy = function(x,y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 	this.x = x;
 	this.y = y;
 	this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Enemy update method
 Enemy.prototype.update = function(dt) {
     this.x += (this.speed * dt);
     
     //This will vary the speed for each enemy
     var spd = speed[Math.floor(Math.random() * speed.length)];
     
-    //This checks to see if enemy if off screen, if so it will
+    //This checks to see if enemy has moved off canvas, if so it will
     //remove it from the array and start a new enemy for that row
     if (this.x > 500) {
     	var index = allEnemies.indexOf(this);
@@ -33,55 +28,90 @@ Enemy.prototype.update = function(dt) {
     }
 }
 
-// Draw the enemy on the screen, required method for game
+// Enemy Render Method
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+//Player class definition
 var Player = function(x,y) {
 	this.x = x;
 	this.y = y;
+	this.direction;       //Direction from inputHandler
+	this.distance = 40;	  //Distance to move
+	this.speed = 100;     //Speed to move
 	this.sprite = 'images/char-boy.png';
 }
 
+//Player update method
 Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Each if statement checks which direction was pressed and
+    // does initial check to make sure player is in bounds.
+    // Another boundary check is performed during for loop.
+   if (this.direction == 'up' && this.y > -10) {
+    	for (i=0;i<this.distance;i++) {
+			this.y -= this.speed * dt;
+			if (this.y < -10) {
+				this.y = -10;
+			}
+		}
+		console.log(this.y);
+	} 			
+	if (this.direction == 'down' && this.y < 385) {
+		for (i=0;i<this.distance;i++) {
+			this.y += this.speed * dt;
+			if (this.y > 385) {
+				this.y = 385;
+			}
+		}
+		console.log(this.y);
+	} 
+	if (this.direction == 'left' && this.x > 0) {
+		for (i=0;i<this.distance;i++) {
+			this.x -= this.speed * dt;
+			if (this.x < 0) {
+				this.x = 0;
+			}
+		}
+		console.log(this.x);
+	} 
+	if (this.direction == 'right' && this.x < 405) {
+		for (i=0;i<this.distance;i++) {
+			this.x += this.speed * dt;
+			if (this.x > 405) {
+				this.x = 405;
+			}
+		}
+		console.log(this.x);
+	}
+    this.direction = ' ';  //Resets direction to prevent endless moves
 }
 
+//Player render method
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-Player.prototype.handleInput = function() {
-	
+//Player input handler from user
+Player.prototype.handleInput = function(key) {
+	this.direction = key; 
 }
 
 // Supporting variables for enemies
-var allEnemies = [];
-var startX = -110;         //Enemy starting point, off canvas
+var allEnemies = [];        //Enemy array
+var startX = -110;          //Enemy starting point, off canvas
 var speed = [60,110,150];   //Enemy speed options
 var row1 = 60;
 var row2 = 143;
 var row3 = 226;
 
-//Instantiate enemies
+//Instantiate enemies on various rows and speeds
 allEnemies.push(new Enemy(startX,row1,speed[0]));
 allEnemies.push(new Enemy(startX,row2,speed[1]));
 allEnemies.push(new Enemy(startX,row3,speed[2]));
 
-// Place the player object in a variable called player
+// Place object with starting position
 var player = new Player(201,390);
-
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
